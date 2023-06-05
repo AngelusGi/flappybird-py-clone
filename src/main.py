f3237e0a8c5311021ac47962a16f5f2ea0c99633
angelusgi <agv.dev@outlook.it>
2023-06-05 10:58:45 +0200

    update path management

--- src/main.py
+++ src/main.py
@@ -7,13 +7,15 @@ from pygame.locals import *
 window_width = 640
 window_height = 480
 coordinates = (window_width, window_height)
+image_asset_path = 'src/assets/images'
 
 ### set height and width of window
 window = pygame.display.set_mode(coordinates)
 # elevation = window_height * 0.8
 fps = 32
-pipe_image_path = 'src/assets/images/pipe.png'
-background_image_path = 'src/assets/images/background.jpg'
-birdplayer_image_path = 'src/assets/images/bird.png'
-sealevel_image_path = 'src/assets/images/base.jfif'
+pipe_image_path = f'{image_asset_path}/pipe.png'
+background_image_path = f'{image_asset_path}/background.jpg'
+birdplayer_image_path = f'{image_asset_path}/bird.png'
+sealevel_image_path = f'{image_asset_path}/base.jfif'
+game_images = {}
 
