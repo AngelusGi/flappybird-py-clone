67de743b1b15c246123ff50862785a22e5ed4c79
angelusgi <agv.dev@outlook.it>
2023-06-05 10:59:01 +0200

    add main function

--- src/main.py
+++ src/main.py
@@ -19,3 +19,37 @@ birdplayer_image_path = f'{image_asset_path}/bird.png'
 sealevel_image_path = f'{image_asset_path}/base.jfif'
 game_images = {}
 
+# program where the game starts
+if __name__ == "__main__":		
+	
+	# For initializing modules of pygame library
+	pygame.init()
+	framepersecond_clock = pygame.time.Clock()
+	
+	# Sets the title on top of game window
+	pygame.display.set_caption('Flappy Bird Game')	
+
+	# Load all the images which we will use in the game
+	# images for displaying score
+	game_images['scoreimages'] = (
+		pygame.image.load(f'{image_asset_path}/0.png').convert_alpha(),
+		pygame.image.load(f'{image_asset_path}/1.png').convert_alpha(),
+		pygame.image.load(f'{image_asset_path}/2.png').convert_alpha(),
+		pygame.image.load(f'{image_asset_path}/3.png').convert_alpha(),
+		pygame.image.load(f'{image_asset_path}/4.png').convert_alpha(),		
+		pygame.image.load(f'{image_asset_path}/5.png').convert_alpha(),
+		pygame.image.load(f'{image_asset_path}/6.png').convert_alpha(),
+		pygame.image.load(f'{image_asset_path}/7.png').convert_alpha(),
+		pygame.image.load(f'{image_asset_path}/8.png').convert_alpha(),
+		pygame.image.load(f'{image_asset_path}/9.png').convert_alpha()
+	)
+	game_images['flappybird'] = pygame.image.load(birdplayer_image_path).convert_alpha()				
+	game_images['sea_level'] = pygame.image.load(sealevel_image_path).convert_alpha()
+	game_images['background'] = pygame.image.load(background_image_path).convert_alpha()
+	game_images['pipeimage'] = (pygame.transform.rotate(pygame.image.load(pipe_image_path)
+														.convert_alpha(),
+														180),
+								pygame.image.load(pipe_image_path).convert_alpha())
+
+	print("WELCOME TO THE FLAPPY BIRD GAME")
+	print("Press space or enter to start the game")
